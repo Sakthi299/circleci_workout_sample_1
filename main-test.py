f141e693fa436162f27bf2f9f4761471cbb3c58d
Sakthi299 <sakthiarun299@gmail.com>
2021-04-28 00:17:51 +0530

    sample function and function-test added

---
+++ main-test.py
@@ -0,0 +1,15 @@
+# Import the Add function, and assert that it works correctly.
+
+# The assert keyword is used when debugging code.
+# The assert keyword lets you test if a condition in your code returns True, if not, the program will raise an AssertionError.
+# You can write a message to be written if the code returns False
+
+from main import Add
+
+def TestAdd():
+    # assert <condition>,<error message>
+    assert Add(2,3) == 5, "sum of 2 and 3 should be 5"
+    print("Add Function works correctly")
+
+if __name__ == '__main__':
+    TestAdd()
